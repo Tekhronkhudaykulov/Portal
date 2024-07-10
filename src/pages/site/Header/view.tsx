d0f7687ef2f5acafe1b8d38a3b59fbb3bdf50cab
Tekhronkhudaykulov <tekhronkhudaykulov@gmail.com>
2024-07-10 11:29:45 +0500

    fix

--- src/pages/site/Header/view.tsx
+++ src/pages/site/Header/view.tsx
@@ -5,9 +5,9 @@ const Header = () => {
   const { t } = useTranslation();
   return (
     <div className="flex items-center justify-between mt-[40px] gap-x-[20px]">
-      <HeaderCard desc={t("done")} />
-      <HeaderCard desc={t("continue")} />
-      <HeaderCard desc={t("allDocument")} />
+      <HeaderCard desc="Обсуждение завершено" />
+      <HeaderCard desc="Обсуждение продолжается" />
+      <HeaderCard desc="Все документы" />
     </div>
   );
 };
