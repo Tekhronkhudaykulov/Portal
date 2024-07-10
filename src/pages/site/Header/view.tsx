7d65dad12121002760bfc4bf90f890d3316ea6f6
Tekhronkhudaykulov <tekhronkhudaykulov@gmail.com>
2024-07-10 11:30:56 +0500

    fix

--- src/pages/site/Header/view.tsx
+++ src/pages/site/Header/view.tsx
@@ -2,7 +2,6 @@ import { useTranslation } from "react-i18next";
 import HeaderCard from "./component/view";
 
 const Header = () => {
-  const { t } = useTranslation();
   return (
     <div className="flex items-center justify-between mt-[40px] gap-x-[20px]">
       <HeaderCard desc="Обсуждение завершено" />
